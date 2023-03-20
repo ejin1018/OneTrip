8ee1551e96909662bba11df5d811a1d57706bfb7
ejin <amy1018@naver.com>
2023-03-20 18:07:36 +0900

    230320 이진 슬라이드 이미지 사이즈 통일

--- src/components/RecomSlider.jsx
+++ src/components/RecomSlider.jsx
@@ -3,13 +3,13 @@ import "swiper/css";
 
 const recommend = [
   {
-    ImgUrl: "/images/countrys2.png",
+    ImgUrl: "/images/countrys3.png",
     destination: "코타키나발루 7일",
     tag: "#하얏트 #5박 이상",
     price: 10000,
   },
   {
-    ImgUrl: "/images/countrys2.png",
+    ImgUrl: "/images/countrys1.png",
     destination: "오키나와 2일",
     tag: "#온천 #1박 이상",
     price: 10000,
@@ -30,12 +30,13 @@ const recommend = [
 
 const RecomSlider = () => {
   return (
-    <Swiper className="recom-wrap" spaceBetween={50} slidesPerView={3} onSlideChange={() => console.log("slide change")} onSwiper={(swiper) => console.log(swiper)}>
+    <Swiper className="recom-wrap" spaceBetween={30} slidesPerView={2.5} onSlideChange={() => console.log("slide change")} onSwiper={(swiper) => console.log(swiper)}>
       {recommend &&
         recommend.map((value, idx) => {
+          const recomBack = {background:`no-repeat center/cover url(${recommend[idx].ImgUrl})`}
           return (
             <SwiperSlide className="recom-box">
-              <img src={recommend[idx].ImgUrl} alt="" />
+              <div className="recom-img" style={recomBack}></div>
               <div>
                 <p>{recommend[idx].destination}</p>
                 <p>{recommend[idx].tag}</p>
