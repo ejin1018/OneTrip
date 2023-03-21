4d2ab340346961350ebe1ff32a4527078d2716cf
ejin <amy1018@naver.com>
2023-03-21 18:08:53 +0900

    230321 이진 국가별 영역 Country 컴포넌트로 분리 후 재분류, 이미지 등록

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,50 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Main.scss";
+
 const country = [
   {
-      ImgUrl:"/images/main.png",
-      text:"한국"
+      ImgUrl:"/images/anywhere-jeju.png",
+      text:"제주"
   },
   {
-      ImgUrl:"/images/main.png",
-      text:"호주"
+      ImgUrl:"/images/anywhere-gangwon.png",
+      text:"강원"
   },
   {
-      ImgUrl:"/images/main.png",
-      text:"영국"
+      ImgUrl:"/images/anywhere-busan.png",
+      text:"경상"
   },
   {
-      ImgUrl:"/images/main.png",
-      text:"독일"
+      ImgUrl:"/images/anywhere-jeonju.png",
+      text:"전라"
   },
   {
-      ImgUrl:"/images/main.png",
-      text:"일본"
+      ImgUrl:"/images/anywhere-singapore.png",
+      text:"동남아시아"
   },
   {
-      ImgUrl:"/images/main.png",
-      text:"대만"
+      ImgUrl:"/images/anywhere-paris.png",
+      text:"유럽"
   },
   {
-      ImgUrl:"/images/main.png",
-      text:"베트남"
+      ImgUrl:"/images/anywhere-america.png",
+      text:"아메리카"
   },
   {
-      ImgUrl:"/images/main.png",
-      text:"스위스"
+      ImgUrl:"/images/anywhere-vacation.png",
+      text:"휴양지"
   },
 ] 
+
+
 function Country(){
   return(
       <div className="countrys">
-        <h3 className="section-title">국가별</h3>
+        <h3 className="section-title">어디든, 원트립</h3>
           <div className="country-wrap">
             {country &&
               country.map((value, idx) => {
+                const countryBack = {background:`no-repeat center/cover url(${country[idx].ImgUrl})`}
                 return (
-                 <div className="country">
-                    <img src={country[idx].ImgUrl} alt="" />
+                  <Link className="country" to={"/"}>
+                    <div className='country-img' style={countryBack}></div>
                     <p>{country[idx].text}</p>
-                  </div>
+                  </Link>
                 );
               })
             }
