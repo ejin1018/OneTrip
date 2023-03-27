50c3dbf895fc71c495d3673ca578b9bc03b20fac
Youngjung3 <youngj5396@gmail.com>
2023-03-27 15:51:26 +0900

    230327 영중 특가부분 DB연동

--- src/components/SpecialsSlider.jsx
+++ src/components/SpecialsSlider.jsx
@@ -1,8 +1,11 @@
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import axios from "axios";
 import "swiper/css";
+import { API_URL } from "./config/constants";
 import { SwapOutlined } from "@ant-design/icons";
 
-const recommend = [
+/* const recommend = [
   {
     ImgUrl: "/images/special.png",
     start: "서울",
@@ -44,30 +47,42 @@ const recommend = [
     edate: "4월 10일 (수)",
   },
 ];
-
+ */
 const SpecialsSlider = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    let url = `${API_URL}/productdate`;
+    axios
+      .get(url)
+      .then((result) => {
+        const products = result.data.product;
+        setProducts(products);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <Swiper className="specials-wrap" spaceBetween={20} slidesPerView={2.3}>
-      {recommend &&
-        recommend.map((value, idx) => {
+      {products.map((value, idx) => {
           return (
             <SwiperSlide className="specials-box">
               {/* <div className="soldout">예약마감</div> */}
-              <img src={recommend[idx].ImgUrl} alt="" className="specials-img" />
+              <img src={`${API_URL}/${value.imageUrl}`} alt="" className="specials-img" />
               <div className="specials-text">
                 <div className="specials-area">
                   <p className="specials-where">
-                    {recommend[idx].start}
+                    {value.departure}
                     <span>
                       <SwapOutlined />
                     </span>
-                    {recommend[idx].end}
+                    {value.redeparture}
                   </p>
-                  <p className="specials-price">최저 <span>{recommend[idx].price} 원</span></p>
+                  <p className="specials-price">최저 <span>{value.price} 원</span></p>
                 </div>
                 <div className="specials-date">
-                  <p>{recommend[idx].sdate}</p>
-                  <p>{recommend[idx].edate}</p>
+                  <p>{value.p_sdate}</p>
+                  <p>{value.p_edate}</p>
                 </div>
               </div>
             </SwiperSlide>
