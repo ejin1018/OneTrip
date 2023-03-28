d2774ec7f5dd1ab85484c36827fc6897564d1712
KMJ_DEV~ <dds13462@gmail.com>
2023-03-28 18:22:08 +0900

    230328 민정 리뷰페이지 생성

--- src/components/RecomSlider1.jsx
+++ src/components/RecomSlider1.jsx
@@ -2,12 +2,9 @@ import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import axios from "axios";
 import { API_URL } from "./config/constants";
-import { Link } from "react-router-dom";
 import "swiper/css";
 const RecomSlider1 = () => {
   const [products, setProducts] = useState([]);
-  const [items, setItems] = React.useState(products);
-
   useEffect(() => {
     let url = `${API_URL}/producttheme`;
     axios
@@ -15,14 +12,10 @@ const RecomSlider1 = () => {
       .then((result) => {
         const products = result.data.product;
         setProducts(products);
-        setItems(products);
       })
       .catch((error) => {
         console.log(error);
       });
-    return()=>{
-      setProducts(products);
-    }
   }, []);
   const CatValues = [
     "아무거나",
@@ -33,9 +26,10 @@ const RecomSlider1 = () => {
     ),
   ];
   // const [products, setProducts] = useState([]);
+  const [items, setItems] = React.useState(products);
 
-  // console.log(items);
-  // if(items.length==0){return};
+  console.log(items);
+  if(items.length==0){return};
 
   // console.log(items);
   // if(products==null){return}
@@ -54,18 +48,19 @@ const RecomSlider1 = () => {
   };
 
   return (
-    <div /* style={{ marginTop: 300 }} */>
+    <div className="main-contents" style={{ marginTop: 300 }}>
       <div className="select">
         <h2>나는 요즘</h2>
         <select
           onChange={(e) => filterItem(e.target.value)}
-          className="font-bold uppercase pointFont"
+          className="font-bold uppercase"
         >
           {CatValues.map((el, index) => {
+            // console.log(curCat);
             return (
               <option
                 key={el.id}
-                value={el} 
+                value={el} /*  defaultValue={curCat[0]} */
               >
                 {el}
               </option>
@@ -76,11 +71,11 @@ const RecomSlider1 = () => {
       </div>
       <div className="recommends">
         <h3 className="section-title">님을 위한 추천 여행</h3>
+        {/* {console.log(items)}; */}
         <Swiper className="recom-wrap" spaceBetween={20} slidesPerView={2.5}>
+          {/* {console.log(items)}; */}
           {items.map((data, idx) => (
               <SwiperSlide className="recom-box" key={idx}>
-              {data.soldout === 1?<div className="soldout"><p>예약 마감</p></div>:null}
-              <Link className="specials-link" to={`/packages/${data.id}`}>
                 <div className="imgfit-wrap recom-img">
                   <img src={`${API_URL}/${data?.imageUrl}`} alt="" />
                 </div>
@@ -92,7 +87,6 @@ const RecomSlider1 = () => {
                     <span>{data?.price}</span> 원 ~
                   </p>
                 </div>
-                </Link>
               </SwiperSlide>
             ))}
         </Swiper>
