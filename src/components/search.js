04f8500c386487240f5f51f40f19bd7a4a1773ac
Youngjung3 <youngj5396@gmail.com>
2023-03-28 16:58:20 +0900

    230328 영중 품절연동, 테마선택수정

--- src/components/search.js
+++ src/components/search.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { API_URL } from "./config/constants";
 
-import { Input, Select } from "antd";
 const Search = () => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
@@ -22,17 +21,53 @@ const Search = () => {
     "모두",
     ...new Set(
       products.map((el) => {
+        console.log(el.theme);
         return el.theme;
       })
     ),
   ];
-  
-  //if(products==null){return}
- console.log(products);
+  const AllItem = [
+    "모두",
+    ...new Set(
+      products.map((el) => {
+        console.log(el.theme);
+        return el.theme;
+      })
+    ),
+  ];
+  const [datas, setDatas] = React.useState(products);
+  const filterItem = (catItem) => {
+    if (catItem === "모두") {
+      setDatas(products);
+      return;
+    }
+    const updateItems = products.filter((el) => {
+        console.log(el);
+      return el.theme == catItem;
+    });
+   
+    setDatas(updateItems);
+    console.log(datas,products);
+  };
   return (
-    <select>
-      <option />
-    </select>
+    <div style={{height:500}}>
+      <div id="body">
+        <h1>Search</h1>
+        <select onChange={(e) => filterItem(e.target.value)}>
+          {AllCat.map((el, idx) => {
+            return (
+              <option key={el.id} value={el}>
+                {el}
+              </option>
+            );
+          })}
+        </select>
+        {datas.map((item, key) => (
+           
+          <div key={key}>{<h3>{item}</h3>}</div>
+        ))}
+      </div>
+    </div>
   );
 };
 export default Search;
