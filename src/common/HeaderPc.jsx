d00d3b2623ae54d3ecd89d297dbb672e1766e32d
ejin <amy1018@naver.com>
2023-03-29 12:22:26 +0900

    230329 이진 common 파일 폴더 정리, 한 번에 취합 후 app.js에 적용

--- src/common/HeaderPc.jsx
+++ src/common/HeaderPc.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Menu } from 'antd';
-import './Main.scss';
+import '../components/Main.scss';
 
 function getItem(label, key, icon, children, type) {
   return {
