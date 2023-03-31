8d3f0e2e3ea1919db041ae85abdb559fd0615e42
Youngjung3 <youngj5396@gmail.com>
2023-03-31 16:29:17 +0900

    230331 영중 잔여수량구현

--- src/common/HeaderPc.jsx
+++ src/common/HeaderPc.jsx
@@ -1,49 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Menu } from 'antd';
+import { API_URL } from "../components/config/constants";
+import axios from "axios";
 import '../sass/Header.scss';
 
-function getItem(label, key, icon, children, type) {
-  return {
-      key,
-      icon,
-      children,
-      label,
-      type,
-  };
-}
-const items = [
-  getItem('국내여행지', 'sub1', null, [
-      getItem(null, 'g1', null, [
-          getItem(<Link to={'/productt/서울'}>서울</Link>, '1'),
-          getItem(<Link to={'/productt/경기'}>경기</Link>, '2'),
-          getItem(<Link to={'/productt/강원'}>강원</Link>, '3'),
-          getItem(<Link to={'/productt/충청'}>충청</Link>, '4'),
-          getItem(<Link to={'/productt/전라'}>전라</Link>, '5'),
-          getItem(<Link to={'/productt/경상'}>경상</Link>, '6'),
-          getItem(<Link to={'/productt/제주'}>제주</Link>, '7')
-      ], 'group'),
-  ]),
-  getItem('해외여행지', 'sub2', null, [
-    getItem(null, 'g2', null,[
-      getItem(<Link to={'/productt/동아시아'}>동아시아</Link>, '8'),
-      getItem(<Link to={'/productt/동남아시아'}>동남아시아</Link>, '9'),
-      getItem(<Link to={'/productt/서유럽'}>서유럽</Link>, '10'),
-      getItem(<Link to={'/productt/동유럽'}>동유럽</Link>, '11'),
-      getItem(<Link to={'/productt/아메리카'}>아메리카</Link>, '12'),
-      getItem(<Link to={'/productt/라틴아메리카'}>라틴아메리카</Link>, '13'),
-      getItem(<Link to={'/productt/휴양지'}>휴양지</Link>, '14')
-    ],'group')
-  ]),
-  // getItem(null, 'sub3', null, [
-  //   getItem('검색', '15'), 
-  //   getItem('리뷰', '16'), 
-  //   getItem('찜', '17')
-  // ], 'group')
-];
-const rootSubmenuKeys = ['sub1', 'sub2', 'sub3'];
 
 const HeaderPc = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    let url = `${API_URL}/products`;
+    axios
+      .get(url)
+      .then((result) => {
+        const products = result.data.product;
+        setProducts(products);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  function getItem(label, key, icon, children, type) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+        type,
+    };
+  }
+  // console.log(products,"fdafafad")
+  const items = [
+    getItem('국내여행지', 'sub1', null, [
+      getItem(null, 'g1', null, [
+            getItem(<Link to={`/productt/${products.p_area}`}>{products.p_area}</Link>, '1'),
+            getItem(<Link to={'/productt/경기'}>경기</Link>, '2'),
+            getItem(<Link to={'/productt/강원'}>강원</Link>, '3'),
+            getItem(<Link to={'/productt/충청'}>충청</Link>, '4'),
+            getItem(<Link to={'/productt/전라'}>전라</Link>, '5'),
+            getItem(<Link to={'/productt/경상'}>경상</Link>, '6'),
+            getItem(<Link to={'/productt/제주'}>제주</Link>, '7')
+        ], 'group'),
+    ]),
+    getItem('해외여행지', 'sub2', null, [
+      getItem(null, 'g2', null,[
+        getItem(<Link to={'/productt/동아시아'}>동아시아</Link>, '8'),
+        getItem(<Link to={'/productt/동남아시아'}>동남아시아</Link>, '9'),
+        getItem(<Link to={'/productt/서유럽'}>서유럽</Link>, '10'),
+        getItem(<Link to={'/productt/동유럽'}>동유럽</Link>, '11'),
+        getItem(<Link to={'/productt/아메리카'}>아메리카</Link>, '12'),
+        getItem(<Link to={'/productt/라틴아메리카'}>라틴아메리카</Link>, '13'),
+        getItem(<Link to={'/productt/휴양지'}>휴양지</Link>, '14')
+      ],'group')
+    ]),
+    // getItem(null, 'sub3', null, [
+    //   getItem('검색', '15'), 
+    //   getItem('리뷰', '16'), 
+    //   getItem('찜', '17')
+    // ], 'group')
+  ];
+  const rootSubmenuKeys = ['sub1', 'sub2', 'sub3'];
+  
   const [openKeys, setOpenKeys] = useState(['sub1']);
   const onOpenChange = (keys) => {
       const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
